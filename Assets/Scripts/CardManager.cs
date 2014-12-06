e93631a05dcb103885aec6f13213242e18530603
Cheryl Savery <cheryl.savery@gmail.com>
2014-12-06 15:03:18 -0500

    CardManager

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CardManager : MonoBehaviour {
+
+
+	Stack<Card> CardStack = new Stack<Card>();
+
+	void Awake ()
+	{
+		
+		
+		
+		GetComponent<Clickable>().DownAction += OnDownAction;
+		GetComponent<Clickable>().UpAction += OnUpAction;
+	}
+	
+	void OnDownAction (Vector3 position)
+	{
+		
+	//	drag = false;
+		
+	}
+	
+	void OnUpAction (Vector3 position)
+	{
+	//	drag = false;
+	}
+
+
+
+	public void AddToStack(Card card)
+	{
+		CardStack.Push(card);
+		//card.EnableAllColliders();
+	}
+
+	public void RemoveFromStack()
+	{
+
+		Card card = CardStack.Pop();
+		//card.DisableAllColliders();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
