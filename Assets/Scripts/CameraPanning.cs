9bd7ddaa3d0fa4edbc8d11f9233b1008aa6ae193
Cheryl Savery <cheryl.savery@gmail.com>
2014-12-07 12:04:32 -0500

    Added zooming and improved panning

--- Assets/Scripts/CameraPanning.cs
+++ Assets/Scripts/CameraPanning.cs
@@ -6,15 +6,28 @@ public class CameraPanning : MonoBehaviour {
 	Vector3 prevMousePos;
 
 	// limits for panning
-	int minX = -4;
-	int minY = -2;
-	int maxX = 4;
-	int maxY = 2;
+
+	float zoomSpeed = 8.0f;
+	float minSize = 6.0f;
+	float maxSize = 16.0f;
+	float startSize = 8.0f;
+	float startMinX = -8;
+	float startMinY = -8;
+	float startMaxX = 8;
+	float startMaxY = 8;	
+	float minX;
+	float minY;
+	float maxX;
+	float maxY;
 
 	void Awake ()
 	{
 	
-		
+		camera.orthographicSize = startSize;
+		minX = startMinX;
+		minY = startMinY;
+		maxX = startMaxX;
+		maxY = startMaxY;
 		GetComponent<Clickable>().DownAction += OnDownAction;
 		InputManager.Instance.GlobalUpAction += OnUpAction;
 	}
@@ -22,7 +35,7 @@ public class CameraPanning : MonoBehaviour {
 	void OnDownAction (Vector3 position)
 	{
 		
-		panning = false;
+		panning = true;
 		
 	}
 	
@@ -58,5 +71,33 @@ public class CameraPanning : MonoBehaviour {
 			this.transform.position = newCameraPosition;
 		}
 		prevMousePos = Input.mousePosition;
+		if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Plus)|| Input.GetKey(KeyCode.Equals)|| Input.GetAxis("Mouse ScrollWheel") < 0)
+		{
+			if (camera.orthographicSize > minSize)
+			{
+				camera.orthographicSize -= Time.deltaTime * zoomSpeed;
+				minX = startMinX - startSize  + camera.orthographicSize;
+				maxX = startMaxX + startSize - camera.orthographicSize;
+				minY = startMinY - startSize  + camera.orthographicSize;
+				maxY = startMaxY + startSize - camera.orthographicSize;
+			}
+		}
+		else if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Minus) || Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			if (camera.orthographicSize < maxSize)
+			{
+			    camera.orthographicSize += Time.deltaTime * zoomSpeed;
+				minX = startMinX - startSize  + camera.orthographicSize;
+				maxX = startMaxX + startSize - camera.orthographicSize;
+				minY = startMinY - startSize  + camera.orthographicSize;
+				maxY = startMaxY + startSize - camera.orthographicSize;
+			}
+		}
+
+//		if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+//		{
+//			camera.orthographicSize = Mathf.Min(Camera.main.orthographicSize-1, 6);
+//		}
+
 	}
 }
